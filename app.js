ec9c49071c996fc22561c9a60566a1162f1b1f4a
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2022-12-10 02:57:33 +0530

    👌 IMPROVE:

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-
-var con = require('./database.js')
-var {rou , io} = require('./router.js')
-
-
-
-
-
-
-
-
+var con = require("./database.js");
+var { rou, io , client } = require("./router.js");
 
 let data_n = ["q1"];
 let data_m = ["hey"];
@@ -17,16 +8,8 @@ let data_f = ["please"];
 let data_op = ["yes"];
 let data_opq = ["q2"];
 
-
-
 var list = [];
 var obj = {};
-
-
-
-
-
-
 
 const {
   Client,
@@ -36,43 +19,9 @@ const {
   LocalAuth,
 } = require("whatsapp-web.js");
 
-const client = new Client({
-  restartOnAuthFail: true,
-  puppeteer: {
-    headless: true,
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--disable-accelerated-2d-canvas",
-      "--no-first-run",
-      "--no-zygote",
-      "--single-process", // <- this one doesn't works in Windows
-      "--disable-gpu",
-      "--use-gl=egl",
-    ],
-  },
-  authStrategy: new LocalAuth(),
-});
 
-client.initialize();
 
-io.on("connection", function (socket) {
-  let code;
 
-  client.on("qr", (qr) => {
-    console.log("QR RECEIVED", qr);
-    code = qr;
-    code.toDataURL(qr, (err, url) => {
-      socket.emit("tx", url);
-    });
-  });
-
-  client.on("ready", () => {
-    socket.emit("ready", "Hii bot is ready");
-    console.log("Client is ready!");
-  });
-});
 
 //assuming we only have 1 chat
 
@@ -128,8 +77,12 @@ function find_op(m) {
 
 client.on("message", async (msg) => {
   console.log("MESSAGE RECEIVED", msg.body);
+  con.query('SELECT op1 , op2  , op3 FROM questions', function (err, rows, fields) {
+    if (err) throw err;
+    console.log('The solution is: ', rows[0].solution);
+  });
 
-  message;
+
 
   // let mes = " ";
 
@@ -167,64 +120,6 @@ client.on("message", async (msg) => {
   //   client.sendMessage(msg.from, button);
   //   }
 });
-
-
-
-// app.get("/", function (req, res) {
-//   let data = []
-//   con.query("SELECT name, message FROM questions WHERE user=?",["raj"],
-//   function (err, result, fields ) {
-//     if (err) throw err;
-    
-//     result.forEach(element => {
-//         Object.values(element)[0]
-       
-//        console.log(data);
-//     });
-    
-//     }
-//   );
-//   res.render("index", {
-//     data: data_n,
-//     message: data_m,
-//     user_data: data,
-//   });
-// });
-// var mysql = require("mysql");
-const { name } = require("browser-sync");
-
-// var con = mysql.createConnection({
-//   host: "181.215.79.245",
-//   user: "rajvendra_admin",
-//   password: "Kamingo@1111",
-//   database: "admin_sugardb",
-// });
-
-
-function insert_questions(
-  name,
-  tittle,
-  message,
-  footer,
-  op1,
-  op2,
-  op3,
-  op1_q,
-  op2_q,
-  op3_q,
-  user
-) {
-  var sql =
-    "INSERT INTO questions (name , message , tittle ,footer , op1 , op2 , op3 , op1_q , op2_q , op3_q , user ) VALUES ?";
-  var values = [
-    [name, message, tittle, footer, op1, op2, op3, op1_q, op1_q, op3_q, user],
-  ];
-
-  con.query(sql, [values], function (err, result) {
-    if (err) throw err;
-    console.log("Number of records inserted: " + result.affectedRows);
-  });
-}
 
 
 
--- router.js
+++ router.js
@@ -1,4 +1,5 @@
 
+const EventEmitter = require('events')
 var express = require('express')
 const http = require("http");
 var con = require('./database.js')
@@ -8,6 +9,32 @@ const socketIO = require("socket.io");
 const server = http.createServer(app);
 const port = process.env.PORT || 8000;
 const io = socketIO(server);
+const emitter = new EventEmitter();
+
+
+
+const {Client,LocalAuth, MessageMedia , Buttons} = require("whatsapp-web.js");
+const client = new Client({
+  restartOnAuthFail: true,
+  puppeteer: {
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-accelerated-2d-canvas",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process", // <- this one doesn't works in Windows
+      "--disable-gpu",
+      "--use-gl=egl",
+    ],
+  },
+  authStrategy: new LocalAuth(),
+});
+
+client.initialize();
+
 
 app.use(express.urlencoded());
 app.io = io;
@@ -15,47 +42,65 @@ app.io = io;
 
 app.set("view engine", "ejs");
 
-app.get('/', function (req, res, next) {
-  let data = []
-   con.query("SELECT name, message FROM questions WHERE user=?",["raj"],
-    function (err, result, fields) {
-      res.render('index' , {data : result})
-    }
-      
-    
-  );
- 
- 
- 
-})
-
-app.post("/", function (req, res) {
-  res.redirect("/");
-
-  let data = req.body;
-  insert_questions(
-    data.name,
-    data.question,
-    data.question_title,
-    data.question_footer,
-    data.op1,
-    data.op2,
-    data.op3,
-    data.op1_q,
-    data.op2_q,
-    data.op3_q,
-    "raj"
-  );
-
-  console.log(data);
+// app.use('/', (req, res) => {
+//   let code;
+client.on("qr", (qr) => {
+  console.log("QR RECEIVED", qr);
+  console.log(qr);
+  var sql =
+  "INSERT INTO client (qr) VALUES ?";
+  var values = [[qr]];
+  con.query(sql, [values], function (err, result) {
+  if (err) throw err;
+  console.log("Number of records inserted: " + result.affectedRows);
 });
 
 
+});
 
+// });
+
+app.get('/', function (req, res, next) {
+  let code
+   con.query("SELECT name, message FROM questions WHERE user=?",["raj"],
+    function (err, result, fields) {
+      res.render('index' , {data : result , qr:code})
+    }
+  );
+})
+
+
+app.post("/", function (req, res) {
+  res.redirect("/");
+  let data = req.body;
+  insert_questions(data.name,data.question,data.question_title,data.question_footer,
+    data.op1,data.op2,data.op3,data.op1_q,data.op2_q,data.op3_q,"raj");
+  console.log(data);
+});
+
+function insert_questions(
+  name,tittle,message,footer,op1,op2,op3,op1_q,op2_q,op3_q,user) {
+  var sql =
+    "INSERT INTO questions (name , message , tittle ,footer , op1 , op2 , op3 , op1_q , op2_q , op3_q , user ) VALUES ?";
+  var values = [[name, message, tittle, footer, op1, op2, op3, op1_q, op2_q, op3_q, user],];
+  con.query(sql, [values], function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+  });
+}
+
+
+io.on("connection", function (socket) {
+  
+  client.on("ready", () => {
+    socket.emit("ready", "Hii bot is ready");
+    console.log("Client is ready!");
+  });
+});
 
 
 server.listen(port, function () {
   console.log("App running on *: " + port);
 });
 
-module.exports = {router , io}
+module.exports = {router , io , client}
