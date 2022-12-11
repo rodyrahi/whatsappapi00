37da8b41ddf87b2546f0c75dfe1a0a0750d65ca9
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2022-12-11 14:49:52 +0530

    kk

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 var con = require("./database.js");
-var { rou, io , client } = require("./router.js");
+var { rou, io, client } = require("./router.js");
 
-let data_n = ["q1"];
-let data_m = ["hey"];
-let data_t = ["hello"];
-let data_f = ["please"];
-let data_op = ["yes"];
-let data_opq = ["q2"];
 
-var list = [];
-var obj = {};
 
 const {
   Client,
@@ -18,154 +10,111 @@ const {
   Buttons,
   LocalAuth,
 } = require("whatsapp-web.js");
+const { localsName } = require("ejs");
 
 
+function find_op(q, msg ) {
+
+  con.query('SELECT * FROM questions', function (err, rows, fields) {
 
 
+    rows.forEach(element => {
+      if (element["name"] === q) {
+        let button = new Buttons(element["message"], [{ body: element["op1"] }, { body: element["op2"] }, { body: element["op3"] }], element["title"], element["footer"]);
+        client.sendMessage(msg.from, button);
+        return
+      }
+    });
+ 
 
-//assuming we only have 1 chat
+  });
+ 
 
-// function find_question(m)
-// {
-//   list.forEach((element) => {
-//     if (m === element[0].message) {
-//       console.log(element[0].message);
-//       return true
-//     }
 
-//   });
+}
+function print(callback) {  
+  let code = "yes"
+  return callback(code);
+}
+print(function(result){
 
-//    return false
-// }
+  console.log(result);
+  //rest of your code goes in here
+});
 
-function find_question(q, m) {
-  data_n.forEach((element) => {});
-  const found = (element) => {
-    if (element === q) {
-      let i = data_n.indexOf(element);
-      console.log(data_m[i]);
-      let button = new Buttons(
-        data_m[i],
-        [{ body: data_op[i] }, { body: data_op2[i] }, { body: "bt3" }],
-        data_t[i],
-        data_f[i]
-      );
-      client.sendMessage(m.from, button);
+function get_data(call){
+      
+  var sql = "SELECT name ,op1 , op2  , op3 , op1_q , op2_q , op3_q FROM questions";
 
-      // m.reply(data_m[i]);
-    }
-  };
-  return data_n.some(found);
+  con.query(sql, function(err, results ){
+        if (err){ 
+          throw err;
+        }
+
+        return call(results);
+    })
+
+ 
 }
 
-function find_op(m) {
-  // checks whether an element is even
-  const found = (element) => {
-    if (element === m.body) {
-      // element === m.body;
-      let i = data_op.indexOf(element);
-      let q = data_opq[i];
-      console.log(q);
-      find_question(q, m);
-      // m.reply(data_m[i]);
-      return true;
-    }
-  };
 
-  return data_op.some(found);
+
+function getKeyByValue(object, value) {
+  return Object.keys(object).find(key => object[key] === value);
 }
+
 
 client.on("message", async (msg) => {
   console.log("MESSAGE RECEIVED", msg.body);
 
-  con.query('SELECT name ,op1 , op2  , op3 , op1_q , op2_q , op3_q FROM questions', function (err, rows, fields) {
-    if (err) throw err;
-    rows.forEach(element => {
+  let found_question = false
 
 
-      
-      function getKeyByValue(object, value) {
-        return Object.keys(object).find(key => object[key] === value);
-      }
-      console.log( getKeyByValue(element , msg.body) );
-      
-      if ( getKeyByValue(element , msg.body) === 'op1' ) {
+
+   let found = get_data( function (results) {
+  
+    results.forEach(element => {
+    
+        if (getKeyByValue(element, msg.body ) === 'op1') {
         let q = element["op1_q"]
-        con.query('SELECT * FROM questions', function (err, rows, fields) {
-          rows.forEach(element => {
-            if ( element["name"] === q ) {
-            let button = new Buttons(element["message"],[{body:element["op1"]},{body:element["op2"]},{body:element["op3"]}],element["title"],element["footer"]);
-            client.sendMessage(msg.from, button);
+        find_op(q, msg)
+        found_question = true
+        return true
+       
+        }
+        else if (getKeyByValue(element, msg.body ) === 'op1') {
+          let q = element["op2_q"]
+          find_op(q, msg)
+          found_question = true
+          return true
+          // console.log(q);
+          }
+        else if (getKeyByValue(element, msg.body ) === 'op1') {
+            let q = element["op3_q"]
+            find_op(q, msg)
+            found_question = true
+            return true
+            // console.log(q);
             }
-          });
-        });
-      }
-      if ( getKeyByValue(element , msg.body) === 'op2' ) {
-        let q = element["op2_q"]
-        con.query('SELECT * FROM questions', function (err, rows, fields) {
-          rows.forEach(element => {
-            if ( element["name"] === q ) {
-            let button = new Buttons(element["message"],[{body:element["op1"]},{body:element["op2"]},{body:element["op3"]}],element["title"],element["footer"]);
-            client.sendMessage(msg.from, button);
-            }
-          });
-        });
-      }
-      if ( getKeyByValue(element , msg.body) === 'op3' ) {
-        let q = element["op3_q"]
-        con.query('SELECT * FROM questions', function (err, rows, fields) {
-          rows.forEach(element => {
-            if ( element["name"] === q ) {
-            let button = new Buttons(element["message"],[{body:element["op1"]},{body:element["op2"]},{body:element["op3"]}],element["title"],element["footer"]);
-            client.sendMessage(msg.from, button);
-            }
-          });
-        });
-      }
-      
+        else{
+          // console.log(false);
+          // return false
+        }
+      return true
+          
     });
+
+    if (! found_question ) {
+      let button = new Buttons("message", [{ body: "op1" }, { body: "op2" }, { body: "op3" }], "title", "footer");
+      client.sendMessage(msg.from, button);
+      }
   });
 
+    console.log(found);
 
+ 
+  })
 
-  // let mes = " ";
-
-  // client
-  // .getChats()
-  // .then((chats) => chats[0].fetchMessages()) //assuming we only have 1 chat
-  // .then((messages) => {
-  //   messages.forEach((element) => {
-  //         if (element.fromMe) {
-  //             mes = element.body
-  //             console.log(mes);
-
-  //             return true
-
-  //         }
-  //   });
-  // });
-
-  // find_op(msg);
-
-  // let button = new Buttons('Button body',[{body:'hello im raj'},{body:'bt2'},{body:'bt3'}],'hey everyone','chose please');
-  // client.sendMessage(msg.from, button);
-
-  // if (!find_op(msg)) {
-  //   let button = new Buttons(
-  //     data_m[0],
-  //     [
-  //       { body: data_op[0] },
-  //       { body: data_op2[0] },
-  //       { body: "Try clicking me", url: "www.google.in" },
-  //     ],
-  //     data_t[0],
-  //     data_f[0]
-  //   );
-  //   client.sendMessage(msg.from, button);
-  //   }
-});
-
-
-
+ 
 
 
