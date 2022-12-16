e3f4910f0caefed28ff6c700cc1b4253bcc78bd2
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2022-12-16 19:50:02 +0530

    hoh

--- database.js
+++ database.js
@@ -4,6 +4,7 @@ var connection = mysql.createConnection({
   user: "raj_sugardb",
   password: "Kamingo@11",
   database: "raj_sugardb",
+  charset:"utf8mb4"
 });
 connection.connect((err) => {
   if (err) {
