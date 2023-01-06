cb2950f2aa93178532aec987b3d6323a3b40cf20
trivendra rahi <62616657+trakagreat@users.noreply.github.com>
2023-01-05 22:01:38 -0800

    host fix

--- database.js
+++ database.js
@@ -1,8 +1,15 @@
+var Host  = "165.232.151.6" 
 var mysql = require("mysql");
+var isWin = process.platform === "win32";
 
+if (!isWin) {
+  Host = "localhost"
+  
+}
+console.log(Host);
 
 var connection = mysql.createConnection({
-  host: "165.232.151.6",
+  host: Host,
   user: "raj",
   password: "Kamingo@11",
   database: "raj_sugardb",
